f7d841f3b4e80c11ea335a0977cba0e9bacee046
Daniel Pitek <Daniel813245@outlook.sk>
2021-11-09 13:09:46 +0100

    vyriesene

--- Open-Lab-03.10/Checker.cs
+++ Open-Lab-03.10/Checker.cs
@@ -6,7 +6,14 @@ namespace Open_Lab_03._10
     {
         public int GetNumberOfCharsInString(char letter, string str)
         {
-            throw new NotImplementedException();
+            int nums = 0;
+            foreach (var item in str)
+	        {
+                if(item==letter){
+                    nums++;
+                }
+	        }
+            return nums;
         }
     }
 }
